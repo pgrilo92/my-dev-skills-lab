be6fa40ff7d47b932d4dfba23f3df139e2951e36
Joaquim <pgrilo92@hotmail.com>
2019-11-28 00:15:04 -0500

    initial styling completed

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(function(){
+//     $('#addHome').removeClass('btn-danger').addClass('btn-success')
+//     $('h1').addClass('text-center')
+
+//     let $newLink = $( '<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow.com</a>' );
+
+// })
